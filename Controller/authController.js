8fc93b0a1fc33073908d8bf275f1a9b4fddcef9a
Kshitij Sharma <121818360+kshitijsharma123@users.noreply.github.com>
2023-08-25 13:50:23 +0530

    Update authController.js

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,4 +1,4 @@
-const crypto = require("crypto");
+const   crypto = require("crypto");
 const { promisify } = require("util");
 const User = require("./../models/userModel");
 const jwt = require("jsonwebtoken");
